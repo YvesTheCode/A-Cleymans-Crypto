8c82b622395384ab88aab00398b4d42c7395ee36
YvesTheCode <alexandre.cleymans@gmail.com>
2025-02-28 11:28:40 +0100

    Partie 3 (+ Avancé)

--- crypto_lib_yves_cleymans.js
+++ crypto_lib_yves_cleymans.js
@@ -27,3 +27,27 @@ for (let index = 0; index < string_of_words.length; index++) { //Boucle aussi lo
 }
 return count+1 //On part du principe que ya un mot avant le première espace
 }
+
+////////PARTIE 3/////////
+
+function count_words_by(string_of_words, letter){
+
+let count2 = 0 //Compteur de lettre proffesionnel, demi-frère du compteur d'espace
+
+if (string_of_words === undefined) { //Prévention si paramètre indéfinies
+    string_of_words = 0
+}
+
+for (let index = 0; index < string_of_words.length; index++) { //Boucle aussi longue que la chaine de caractères, non j'ai pas copié-collé
+    
+    if (string_of_words.charAt(index)===letter) {
+            ++count2
+    }
+     
+}
+if(letter != null && letter != ""){ // Compter la lettre du paramètre si y'en a un
+    count2 = count2+1
+}
+
+return count2
+}
