29d0e5457e7645eabce4e95078aeb6d5b8a74d80
YvesTheCode <alexandre.cleymans@gmail.com>
2025-02-28 12:11:30 +0100

    Partie 4 (+Moyen+Avancé)

--- crypto_lib_yves_cleymans.js
+++ crypto_lib_yves_cleymans.js
@@ -51,3 +51,40 @@ if(letter != null && letter != ""){ // Compter la lettre du paramètre si y'en a
 
 return count2
 }
+
+////////PARTIE 4/////////
+
+let vowels = ['a', 'e', 'i', 'u', 'o', 'y', 'A', 'E', 'I', 'U', 'O', 'Y']; // Ici c'est mieux (pour moi au moins)
+
+function  is_a_vowel(letter){
+let voyelle = false
+    for (let index2 = 0; index2 < vowels.length; index2++) {
+        if (letter === vowels[index2]) {
+            voyelle = true
+            break;
+        }
+
+    }
+
+    return voyelle
+}
+
+function count_vowels(string_of_words){
+
+    if (string_of_words === undefined) { //Prévention si paramètre indéfinies
+        string_of_words = 0
+    }
+
+    let count3 = 0 //Compteur de voyelle, aucun lien avec les 2 précédents compteurs
+    for (let index = 0; index < string_of_words.length; index++) { //Boucle aussi longue que la chaine de caractères, non j'ai pas copié-collé
+    
+    letter = string_of_words.charAt(index)
+    
+    if (is_a_vowel(letter)===true) {
+        ++count3
+
+    }
+  
+    }
+    return count3
+}
