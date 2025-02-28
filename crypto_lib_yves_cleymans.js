4dcc3a513093279a37bb762a65d4c10605ef9c8e
YvesTheCode <alexandre.cleymans@gmail.com>
2025-02-28 11:04:13 +0100

    Partie 2

--- crypto_lib_yves_cleymans.js
+++ crypto_lib_yves_cleymans.js
@@ -11,3 +11,19 @@ function what_are_words_worth(){
     return valeur_entree.length //Pour eviter l'erreur je retourne une valeur
 
 }
+
+////////PARTIE 2/////////
+
+function count_words(string_of_words){
+
+let count = 0 //Compteur d'espace
+
+for (let index = 0; index < string_of_words.length; index++) { //Boucle aussi longue que la chaine de caractères
+    
+    if (string_of_words.charAt(index)===" ") {
+        ++count
+    }
+ 
+}
+return count+1 //On part du principe que ya un mot avant le première espace
+}
